9427f2ccfd16d0d70f43d7bbdffc1202fb377151
claraszw <clara_szw@hotmail.com>
2014-09-21 12:20:52 -0300

    função criança
    - botei uma funçãozinha de converter que eu testei já e parece estar
    funcionando
    - adicionei a leitura com o fread

--- utf8-32.c
+++ utf8-32.c
@@ -1,64 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned int conv_litte_endian(unsigned int n)
+{
+	unsigned int aux, saida, mask=0xFF;
+	int i;
+
+	saida=0x0;
+
+	for(i=0;i<4;i++)
+	{
+		aux= mask & n;
+		aux<<=(24-8*i);
+		saida= saida | aux;
+		n>>=8;
+	}
+
+	return saida;
+}
+
 int conv8_32(FILE *arq_entrada, FILE *arq_saida, char ordem)
 {
-	int saida, aux;
+	unsigned int saida, aux;
 	int num_byte =1;
 	unsigned char byte_lido;
 	int qtd_bytes=1;
 	int i;
 	
-	while(/*houver algo no arquivo*/0)
+	while(fread(&byte_lido,1,1,arq_entrada)==1)
 	{
 
 
-	// le byte
-
-	while(0x8000 & byte_lido)
-	{
-		qtd_bytes++;
-
-		byte_lido<<=1;
-	}
-
-	if(qtd_bytes==1)
-		saida=(0x0 | byte_lido);
-
-	else
-	{
-		aux=byte_lido;
-
-		aux<<=(4*(qtd_bytes-2));
-
-		saida=(0x0 | aux);
-
-		for(i=0;i<qtd_bytes;i++)
+		while(0x8000 & byte_lido) // verifica quantos bytes serão necessários
 		{
-			// le proximo byte
+			qtd_bytes++;
 
-			//checa se byte esta correto
+			byte_lido<<=1;
+		}
 
-			if(!(0x8000 & byte_lido) || (0x4000 & byte_lido) ) // verifica se o primeiro byte é 1 e se o segundo é 0
-			{
-			  printf("Erro no byte %d",num_byte);
-			  return -1;
-			}
+		if(qtd_bytes==1)
+			saida=(0x0 | byte_lido);
 
+		else
+		{
 			aux=byte_lido;
 
-			aux=(aux & 0x7FFF);  // zera primeiro byte
+			aux<<=(4*(qtd_bytes-2));
 
-			aux<<=(6*(qtd_bytes-2-i)); // shifta até a posição correta
+			saida=(0x0 | aux);
 
-			saida=(saida | aux);
+			for(i=0;i<qtd_bytes;i++)
+			{
+				
+				fread(&byte_lido,1,1,arq_entrada);  // le proximo byte
+
+
+				if(!(0x8000 & byte_lido) || (0x4000 & byte_lido) ) // verifica se o primeiro byte é 1 e se o segundo é 0
+				{
+					printf("Erro no byte %d",num_byte);
+					return -1;
+				}
+
+				aux=byte_lido;
+
+				aux=(aux & 0x7FFF);  // zera primeiro byte
+
+				aux<<=(6*(qtd_bytes-2-i)); // shifta até a posição correta
+
+				saida=(saida | aux);
+			}
 		}
-	}
 
 		
-		//imprime byte, se necessário, converter de big pra little
+		if(ordem=='l')//se necessário, converter de big pra little
+		{
+			saida=conv_litte_endian(saida);
+		}
+
+
+		fwrite(&saida,4,1,arq_saida); //imprime
 
 		num_byte++;
 
 	}
+
+	return 0;
 }
